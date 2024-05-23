adf2142d1270dcba80a713e2043d2626402f57ea
Bab Codes <113517332+bab882@users.noreply.github.com>
2024-05-23 16:14:20 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Plugin for custom canvas background color
 const backgroundPlugin = {
   id: 'customCanvasBackgroundColor',
   beforeDraw: (chart, args, options) => {
@@ -10,56 +11,96 @@ const backgroundPlugin = {
 };
 Chart.register(backgroundPlugin);
 
+// Configuration du graphique
 const ctx = document.getElementById('myChart').getContext('2d');
 const myChart = new Chart(ctx, {
   type: 'bar',
   data: {
-      labels: ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
+      labels: ['Lundi'],
       datasets: [{
-          label: 'Nombre Tickets',
-          data: [70, 40, 38, 55, 100, 67],
+          label: 'Fibre',
+          data: [70],
           backgroundColor: [
-            'rgba(54, 162, 235, 0.2)',
+              'rgba(26,174,221,0.7)',
           ],
           borderColor: [
-            'rgba(54, 162, 235, 0.5)',
+              'rgba(54, 162, 235, 0.5)',
           ],
-          borderWidth: 1
+          barPercentage: 0.5,
+          categoryPercentage: 0.5
+      },
+      {
+          label: 'CPE',
+          data: [33],
+          backgroundColor: [
+              'rgba(54, 162, 235, 0.2)',
+          ],
+          borderColor: [
+              'rgba(54, 162, 235, 0.5)',
+          ],
+          barPercentage: 0.5,
+          categoryPercentage: 0.5,
       }]
   },
   options: {
       scales: {
-        x: {
-          display: true,
-          grid: {
-              color: 'rgba(200, 200, 200, 0.2)',
-              lineWidth: 1,
-              borderDash: [5, 5],
-              borderDashOffset: 0,
-              drawBorder: true 
-          },
-          title: {
+          x: {
               display: true,
-              text: 'Semaine'
-          }
-      },
+              grid: {
+                  color: 'rgba(200, 200, 200, 0.2)',
+                  lineWidth: 1,
+                  borderDash: [5, 5],
+                  borderDashOffset: 0,
+                  drawBorder: true,
+              },
+              title: {
+                  display: true,
+                  text: 'Ticket'
+              },
+              ticks: {
+                display: false
+              }
+          },
           y: {
               beginAtZero: true,
               max: 100,
               display: true,
-                        grid: {
-                            color: 'rgba(200, 200, 200, 0.2)', 
-                            lineWidth: 1,
-                            borderDash: [5, 5],
-                            borderDashOffset: 0,
-                            drawBorder: true 
-                        },          
+              grid: {
+                  color: 'rgba(200, 200, 200, 0.2)', 
+                  lineWidth: 1,
+                  borderDash: [5, 5],
+                  borderDashOffset: 0,
+                  drawBorder: true 
+              },
+              ticks: {
+                display: false,
+                
+              }
           }
+      },
+      hover: null,
+      interaction: {
+        mode: 'nearest',
+        axis: 'x',
+        intersect: false,
       },
       plugins: {
           customCanvasBackgroundColor: {
               color: '#101517'
+          },
+          datalabels: {
+              anchor: 'center',
+              align: 'center',
+              formatter: function(value, context) {
+                  return value;
+              },
+              color: '#fefefe',
+              font: {
+                  weight: 'bold',
+                  family: 'Roboto'
+              }
           }
       },
-  }
+  },
+  plugins: [ChartDataLabels]
 });
